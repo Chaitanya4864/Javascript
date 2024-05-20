c58ea4775da4a330f2165a9d5af92afa0a03b429
Venkata Raghava Chaitanya <chaitanya4864@gmail.com>
2024-05-20 19:46:17 +0530

    JavaScript second day session

---
+++ day2/Printingstatement.js
@@ -0,0 +1,16 @@
+// console.log("Hi this output is in the console");
+// console.log(console);
+// console.log(45+55);
+// console.log(100*987/45+55%3);
+// console.log(78945/5+17);
+
+// document.write("hi this is on UI1");
+// document.writeln("hi this contains spaces b/w two sentences");
+// document.writeln("hi this contains spaces b/w two sentences");
+// console.log(document);
+
+// document.getElementById("a").innerHTML=" <h1 style='color:red';>this is text</h1>";
+// document.getElementById("c").innerText= " <h1 style='color:red';><center>this is text</center></h1>";
+
+window.alert("this is a Alert box");
+alert("this is a 2nd alert box");
