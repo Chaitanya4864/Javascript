f0ca7aa66b6649b0cfe5610bdf5f54671906f108
Venkata Raghava Chaitanya <chaitanya4864@gmail.com>
2024-05-21 22:07:24 +0530

    Day3 Java-Script Session

---
+++ day3/Tokens.js
@@ -0,0 +1,85 @@
+// var a;
+// console.log(a);
+// let a;
+// console.log(a);
+// const a;
+// console.log(a);
+
+// Declaration and initialisation
+
+// var a=10;
+// console.log(a)
+// let a=10;
+// console.log(a)
+// const a=10;
+// console.log(a)
+
+// first declartion after initialisation
+// var a;
+// a=10;
+// console.log (a)
+// let a;
+// a=20;
+// console.log (a)
+// const a;
+// a=10;
+// console.log (a)
+
+// Reinitialisationvar a;
+//  var a=10;
+// console.log (a)
+// a=20;
+// console.log(a)
+
+// let a=10;
+// console.log (a)
+// a=20;
+// console.log(a)
+// const a=10;
+// console.log (a)
+// a=20;
+// console.log(a)
+
+// Redeclaration
+
+// var a=10;
+// console.log(a)
+// var a=20;
+// console.log(a);
+
+let a=10;
+console.log(a);
+let a=20;
+console.log(a);
+
+// const a=10;
+// console.log(a);
+// const a=20;
+// console.log(a);
+
+// Hoisting  means before initialization we have to print
+
+// console.log(a);
+// var a=10;
+
+// console.log(a);
+// let a=10;
+
+// console.log(a);
+// const a=10;
+
+// Scope
+
+// {
+//     // var a=10; 
+//     // let b=20;
+//     // const c=30;
+
+// }
+//  console.log(b);
+
+// function fun(){
+//     var a=10;
+// }
+// fun();
+// console.log(a);
