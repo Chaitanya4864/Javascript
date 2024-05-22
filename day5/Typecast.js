9bfac3dcffc3139040c047687aaaa32d170a4ef9
Venkata Raghava Chaitanya <chaitanya4864@gmail.com>
2024-05-22 15:27:56 +0530

    Day4 and 5 of Javascript

---
+++ day5/Typecast.js
@@ -0,0 +1,57 @@
+// var str="45apple"
+// console.log(str,typeof str);
+// var num=str*10;
+// console.log(num,typeof num);
+
+
+// concatination
+// var num=10;
+// var str="10";
+// var add =num+str;
+// console.log(add,typeof add);
+
+
+// conversion of string to a number
+
+// var str="45"
+// console.log(str, typeof str);
+// var num=Number(str);
+// console.log(num,typeof num);
+
+// parse int & parseFloat
+// var str="45.5apple"
+// console.log(str, typeof str);
+// var num=parseInt(str);
+// console.log(num, typeof num);
+
+// var str="45.5apple"
+// console.log(str, typeof str);
+// var num=parseFloat(str);
+// console.log(num, typeof num);
+
+// conversion bool to number
+// var bool=true;
+// console.log(bool, typeof bool);
+// var num=Number(bool);
+// console.log(num, typeof num);
+
+// var bool=false;
+// console.log(bool, typeof bool);
+// var num=Number(bool);
+// console.log(num, typeof num);
+
+// conversion string to number
+// var num=45;
+// console.log(num, typeof num);
+// var str=String(num);
+// console.log(str,typeof str);
+
+// var num=45;
+// console.log(num, typeof num);
+// var str=num.toString();
+// console.log(str,typeof str);
+
+var val="false";
+console.log(val,typeof val);
+var bool=Boolean(val);
+console.log(bool, typeof bool);-
