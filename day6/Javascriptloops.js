062eb300c7090066e21a58049bd46a082d8d4699
Venkata Raghava Chaitanya <chaitanya4864@gmail.com>
2024-05-23 17:07:09 +0530

    Day6 of Java Script

---
+++ day6/Javascriptloops.js
@@ -0,0 +1,46 @@
+// for(i=1;i<=10;i++){
+//     console.log(i +"apple")
+// }
+var num=prompt("Enter a number")
+for(i=1;i<10;i++){
+    console.log(num+"x"+i+"="+(num*i));
+}
+// var num=prompt("Enter a number")
+// for(i=10;i>1;i--){
+//     console.log(num+"x"+i+"="+(num*i));
+// }
+
+// Factorial of given number
+// var num=prompt("Enter a number")
+// var fact=1;
+// for(i=1;i<num;i++){
+//     fact*=i;
+// }
+//     console.log(fact);
+
+// while loop
+// var i=1;
+// while(i<=10){
+//     console.log("apple");
+//     i++;
+// }
+
+// var i=1
+// do{
+//     console.log(i+"apple");
+//     i++;
+// }
+// while(i<=15);
+
+// var fact=1;
+// var i=1;
+// var num=4;
+// do{
+//     fact*=i;
+//     i++;
+// }
+// while(i<=num);
+// console.log(fact);
+
+
+   
---
+++ day6/prompt.js
@@ -0,0 +1,7 @@
+var a=prompt("Enter a Number ")
+var b=prompt("Enter a Number")
+// document.getElementById(add).innerHTML="add b/w "a+" and "+b=" is "+ (Number(a)+Number(b));
+// document.getElementById(sub).innerHTML="sub b/w "a+" and "+b+" is"+ (a-b);
+// document.getElementById(mul).innerHTML="mul b/w "a+" and "+b+" is"+ (a*b);
+// document.getElementById(div).innerHTML="div b/w "a+" and "+b+" is"+ (a/b);
+// document.getElementById(mod).innerHTML="div b/w "a+" and "+b+" is"+ (a%b);
