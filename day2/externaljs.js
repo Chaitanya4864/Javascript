baaf56b20c49e2c5a2aa93ef9c2e7387db0ebfbd
Venkata Raghava Chaitanya <chaitanya4864@gmail.com>
2024-06-03 12:10:20 +0530

    Printing Statements notes and small changes in externaljs file

--- day2/externaljs.js
+++ day2/externaljs.js
@@ -1,1 +1,4 @@
-document.getElementById("a").innerHTML="This is Good day"
+document.getElementById("a").innerHTML = "This is Good day"
+document.getElementById("a").innerHTML = `<h1 style="background-color:blue; color:red;">This is Good day</h1>`
+
+document.getElementById("a").innerText = `<h1 style="background-color:blue; color:red;">This is Good day</h1>`
